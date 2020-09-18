9a124cd4455a8c67902d173e2d40b65307061582
Bruno Henrique <brunohvcarvalho@gmail.com>
2020-09-18 11:28:22 -0300

    multi thread

--- BruteForce/BruteForce/Program.cs
+++ BruteForce/BruteForce/Program.cs
@@ -1,33 +1,61 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace BruteForce
 {
-    class Program
+    public class Program
     {
+        public static char[] letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static int[] contLetra = new int[6];
+        public static bool auth = false;
+        public static bool finished = false;
+        public static string senhaFinal = "";
+        public static int quantidadeLetras = 0;
+        public static int quantidadeContLetras = 0;
         static void Main(string[] args)
         {
-            char[] letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            int[] contLetra = new int [6];
             contLetra[0] = 0;
             contLetra[1] = 0;
             contLetra[2] = 0;
             contLetra[3] = 0;
             contLetra[4] = 0;
             contLetra[5] = 0;
-            var auth = false;
-            var finished = false;
-            var senhaFinal = "";
+            quantidadeLetras = letras.Length;
+            quantidadeContLetras = contLetra.Length;
 
+            Console.WriteLine("Inicio: "+ DateTime.Now);
             while (!auth && !finished)
             {
-                var senhaTentativa = String.Format("{0}{1}{2}{3}{4}{5}", 
-                    letras[contLetra[5]], 
-                    letras[contLetra[4]], 
-                    letras[contLetra[3]], 
-                    letras[contLetra[2]], 
-                    letras[contLetra[1]], 
+                Thread t = new Thread(NovaThread);
+                t.Start();
+            }
+            Console.WriteLine("Fim: " + DateTime.Now);
+
+            if (auth)
+            {
+                Console.WriteLine("Senha: " + senhaFinal);
+            }
+            else
+            {
+                Console.WriteLine("Senha não encontrada!");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void NovaThread()
+        {
+            while (!auth && !finished)
+            {
+                var senhaTentativa = String.Format("{0}{1}{2}{3}{4}{5}",
+                    letras[contLetra[5]],
+                    letras[contLetra[4]],
+                    letras[contLetra[3]],
+                    letras[contLetra[2]],
+                    letras[contLetra[1]],
                     letras[contLetra[0]]);
                 var strArguments = String.Format("{0} {1}", "administrador", senhaTentativa);
 
@@ -44,9 +72,9 @@ namespace BruteForce
 
                     process.WaitForExit();
 
-                    Console.WriteLine("Tentativa: " + senhaTentativa + " --> Saída: " + output);
+                    // Console.WriteLine("Tentativa: " + senhaTentativa + " --> Saída: " + output);
 
-                    var validacao = output.Replace("\r\n","").Equals("Login ou senha incorretos!");
+                    var validacao = output.Replace("\r\n", "").Equals("Login ou senha incorretos!");
 
                     if (!validacao)
                     {
@@ -56,23 +84,23 @@ namespace BruteForce
                     }
                     else
                     {
-                        for(var op = 0; op < contLetra.Length; op++)
+                        for (var op = 0; op < quantidadeContLetras; op++)
                         {
-                            if(op == 0)
+                            if (op == 0)
                             {
                                 contLetra[op]++;
                             }
 
-                            if(contLetra[op]+1 > letras.Length)
+                            if (contLetra[op] + 1 > quantidadeLetras)
                             {
-                                if(op != 5)
+                                if (op != 5)
                                 {
                                     contLetra[op + 1]++;
                                     contLetra[op] = 0;
                                 }
                             }
 
-                            if(contLetra[4] == letras.Length && contLetra[5] == letras.Length)
+                            if (contLetra[4] == quantidadeLetras && contLetra[5] == quantidadeLetras)
                             {
                                 Console.WriteLine("FINISHED");
                                 finished = true;
@@ -82,17 +110,6 @@ namespace BruteForce
 
                 }
             }
-
-            if (auth)
-            {
-                Console.WriteLine("Senha: " + senhaFinal);
-            }
-            else
-            {
-                Console.WriteLine("Senha não encontrada!");
-            }
-
-            Console.ReadLine();
         }
     }
 }
